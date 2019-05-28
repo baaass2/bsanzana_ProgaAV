8c0ea781a62d4183d84f86549803c6878e30d5b1
baaass2 <jokerbass087@gmail.com>
2019-05-28 12:24:31 -0400

    Se ha agregado el eje1guia 2

---
+++ guia2e1/src/guia2e1/Comparaciones.java
@@ -0,0 +1,35 @@
+package guia2e1;
+
+public class Comparaciones {
+	
+	private String mano_j1;
+	private String mano_j2;
+	int resultado_j1;
+	int resultado_j2;
+	
+	void metodo_comparaciones() {
+		System.out.println(this.mano_j1);
+		System.out.println(this.mano_j2);	
+	}
+		
+	
+	
+	
+	
+	
+	public String getMano_j1() {
+		return mano_j1;
+	}
+	public void setMano_j1(String mano_j1) {
+		this.mano_j1 = mano_j1;
+	}
+	public String getMano_j2() {
+		return mano_j2;
+	}
+	public void setMano_j2(String mano_j2) {
+		this.mano_j2 = mano_j2;
+	}
+	
+	
+
+}
